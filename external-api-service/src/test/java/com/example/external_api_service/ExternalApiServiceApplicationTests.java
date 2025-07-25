c5a127f9264727fc72258acf80dfc0abaf2b1310
eunjeong Park <eunjeongpark.eundms@gmail.com>
2025-07-25 22:02:45 +0900

    init commit

---
+++ external-api-service/src/test/java/com/example/external_api_service/ExternalApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.external_api_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExternalApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ internal-api-service/src/main/java/com/example/internal_api_service/InternalApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.internal_api_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternalApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternalApiServiceApplication.class, args);
+	}
+
+}
